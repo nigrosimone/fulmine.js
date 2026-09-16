9a6f7acbbbf9bab3cfdffd2d8ecee71fd8be8807
Nigro Simone <nigro.simone@gmail.com>
2026-09-16 19:13:27 +0200

    chore: uws.js is a new file, its header names only its author
    Signed-off-by: Nigro Simone <nigro.simone@gmail.com>

--- src/uws.js
+++ src/uws.js
@@ -1,8 +1,5 @@
 /*
-Copyright 2024 dimden.dev
 Copyright 2026 Nigro Simone
-
-This file is derived from Ultimate Express and has been modified.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,6 +10,9 @@ http://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 // µWebSockets.js, loaded the first time something needs it rather than when this package is
 // required. An application that is built and never listens never loads the binary. Angular's
